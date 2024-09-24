9c7ede79d15ae2465155dc95baad61b65f899afc
Veroonicagp <vgonpon3007@g.educaand.es>
2024-09-24 12:42:56 +0200

    .

---
+++ app/src/main/java/com/example/aplicacion1/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.aplicacion1
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener{
+            val name = findViewById<TextInputEditText>(R.id.textFieldInput).text.toString()
+            Snackbar.make(button, "hola $name", Snackbar.LENGTH_LONG).show()
+        }
+
+    }
+}
